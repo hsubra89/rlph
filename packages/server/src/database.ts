5c5821855843be018ec5e4e8a7f8bf96e24329d8
Harish Subramanium <hsubra89@gmail.com>
2026-03-10 20:51:31 -0700

    Remove unused old code

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -1,16 +1,6 @@
 import { PgClient, PgMigrator } from "@effect/sql-pg"
-import { SqlClient } from "@effect/sql/SqlClient"
-import { Config, Context, Data, Duration, Effect, Layer } from "effect"
+import { Config } from "effect"
 import * as migration0001 from "./migrations/0001_postgres_foundation.js"
-
-export class DatabaseUnavailable extends Data.TaggedError("DatabaseUnavailable")<{
-  readonly cause: unknown
-}> {}
-
-export class DatabaseHealth extends Context.Tag("DatabaseHealth")<
-  DatabaseHealth,
-  { readonly check: Effect.Effect<void, DatabaseUnavailable> }
->() {}
 
 export const PostgresLive = PgClient.layerConfig({
   url: Config.redacted("BRRR_POSTGRES_URL"),
@@ -23,24 +13,6 @@ export const PostgresMigrationsUrl = Config.redacted("BRRR_POSTGRES_MIGRATIONS_U
 export const PostgresMigrationsLive = PgClient.layerConfig({
   url: PostgresMigrationsUrl,
 })
-
-export const DatabaseHealthLive = Layer.effect(
-  DatabaseHealth,
-  Effect.gen(function* () {
-    const sql = yield* SqlClient
-
-    return {
-      check: sql`SELECT 1`.pipe(
-        Effect.asVoid,
-        Effect.mapError((cause) => new DatabaseUnavailable({ cause })),
-        Effect.timeoutFail({
-          duration: Duration.seconds(2),
-          onTimeout: () => new DatabaseUnavailable({ cause: new Error("database health check timed out") }),
-        }),
-      ),
-    }
-  }),
-)
 
 export const runDatabaseMigrations = PgMigrator.run({
   loader: PgMigrator.fromRecord({
